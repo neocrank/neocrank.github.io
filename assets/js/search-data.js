92502cbe3abbf4b5019d78438d05d8daa4589544
neocrank <neocrank3218@gmail.com>
2025-05-06 18:56:37 +0000

    Deploying to gh-pages from @ neocrank/neocrank.github.io@59d0176b03c0ea32f5cbe79b49fd9a7e04aadc37 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -83,6 +83,17 @@ ninja.data = [{
             window.location.href = "/blog/2025/markdown/";
           
         },
+      },{id: "post-라면-맛있게-끓이기",
+        
+          title: "라면 맛있게 끓이기",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/secondpost/";
+          
+        },
       },{id: "post-나의-첫-jekyll-블로그-포스트",
         
           title: "나의 첫 Jekyll 블로그 포스트",
