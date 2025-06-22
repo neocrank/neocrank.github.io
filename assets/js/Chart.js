9f9e77dee410489df57b5e19a7d97aaadb518378
neocrank <neocrank3218@gmail.com>
2025-06-22 19:59:32 +0900

    Create Chart.js

---
+++ assets/js/Chart.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Word Frequency</title>
+</head>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="word-freq.js"></script>
+</body>
+</html>
