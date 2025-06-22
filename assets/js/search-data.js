c5bfe106af0c2dbea50a3547e62f0fd05a4be73e
neocrank <neocrank3218@gmail.com>
2025-06-22 10:24:14 +0000

    Deploying to gh-pages from @ neocrank/neocrank.github.io@f2cda7be52341290a1198ef56687e50a2c046856 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
+        },{id: "nav-word-freq",
+          title: "word-freq",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/word-freq/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
